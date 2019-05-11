c06c5397e87c3a3f1d1d8dcdf08e26f70ec65801
CaesiumY <mn0658@naver.com>
2019-05-11 19:24:29 +0900

    feat: Add template extends

--- TheUBDproject/TheUBD/settings.py
+++ TheUBDproject/TheUBD/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'TheUBD.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['TheUBD/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
